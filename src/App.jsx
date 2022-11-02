54b630ad740f4f05cc544aa1dae9a3db6d8d9761
Timothy H <huynh.tk80@gmail.com>
2022-11-02 11:27:12 -0600

    added count button

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Title, { LittlestComponent, SecondLittlestComponent } from './title.js'
+import { useState } from 'react';
 
-const url = "https://www.google.com"
+// const url = "https://www.google.com"
 //const url2 = "https://reactjs.org"
 
 function App() {
+  const [count, setCount] = useState(0);
+  let testcount = 0;
   return (
     <div className="App">
       <header className="App-header">
-
+        {count}
         <Title />
         <LittlestComponent message={"Hi C9!!"} />
+        <p> You Clicked {count} Times</p>
         <SecondLittlestComponent />
+        <button onClick={()=>setCount(count+1)}>Click Me</button>
+        <button onClick={()=>{testcount = testcount +1; console.log(testcount);console.log(count)}}>Test Click Me</button>
+        <p> You Clicked {testcount} Times</p>
+        
       </header>
+      
     </div>
   );
 }
